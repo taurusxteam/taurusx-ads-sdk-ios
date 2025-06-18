e834139e1b00185f7b47e9264397d0483af81582
jianhui <jianhui.tang@webeye.cn>
2025-06-18 14:05:40 +0800

    1.6.0

--- Adapters/TaxAppLovinAdapter.xcframework/ios-arm64_x86_64-simulator/TaxAppLovinAdapter.framework/Headers/TaurusXALMediationAdapter.h
+++ Adapters/TaxAppLovinAdapter.xcframework/ios-arm64_x86_64-simulator/TaxAppLovinAdapter.framework/Headers/TaurusXALMediationAdapter.h
@@ -7,6 +7,18 @@
 
 #import <AppLovinSDK/AppLovinSDK.h>
 
+typedef NS_ENUM(NSInteger, TaurusXErrorCode) {
+    ERROR_CODE_INVALID_REQUEST = 1,
+    ERROR_CODE_NETWORK_ERROR = 2,
+    ERROR_CODE_NO_FILL = 3,
+    ERROR_CODE_TIMEOUT = 4,
+    ERROR_CODE_SHOW = 5,
+    ERROR_CODE_NO_CONTENT = 6,
+    ERROR_CODE_INTERNAL_ERROR = 7,
+    ERROR_CODE_PARSE_ERROR = 8,
+    ERROR_CODE_DOWNLOAD_ERROR = 9,
+};
+
 @interface TaurusXALMediationAdapter : ALMediationAdapter <MAInterstitialAdapter, MARewardedAdapter, MAAdViewAdapter, MANativeAdAdapter>
 
 @end
