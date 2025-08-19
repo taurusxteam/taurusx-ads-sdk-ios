89a776ebd4fda77b06c9d7712f385ecc394a6e90
jianhui <jianhui.tang@webeye.cn>
2025-08-19 15:07:23 +0800

    1.8.0

--- TaurusxAdsSDK.xcframework/ios-arm64/TaurusxAdsSDK.framework/Headers/TaurusXAds.h
+++ TaurusxAdsSDK.xcframework/ios-arm64/TaurusxAdsSDK.framework/Headers/TaurusXAds.h
@@ -21,26 +21,73 @@ typedef NS_ENUM(NSInteger, TaurusXAdsInitStatus)
 
 @interface TaurusXAds : NSObject
 
+/// Initialization status of the  TaurusX Ads SDK.
 @property (nonatomic, readonly) TaurusXAdsInitStatus initStatus;
 
+/*
+ Returns the shared TaurusXAds instance.
+ */
 + (TaurusXAds *)sharedInstance;
 
+/*
+ Returns the TaurusX Ads SDK's version number.
+ */
 + (NSString*)sdkVersion;
 
+/*
+ Starts the TaurusX Ads SDK. Call this method as early as possible to reduce latency on the
+ session's first ad request. Calls completionHandler when the TaurusX SDK are fully set up.
+ */
 - (void)startWithAppId:(NSString *)appId completionHandler:(void (^ __nullable)(BOOL success))completion;
-//0 设备数据允许上报 ；1 设备数据不允许上报；
+
+/*
+ Set the General Data Protection Regulation (GDPR) value;
+ Must be called before start TaurusX SDK;
+ 0 The device data is allowed to be reported;
+ 1 The device data is not allowed to be reported
+ */
 - (void)setGDPRDataCollection:(NSInteger)level;
-//0 接受上报数据；1 加州用户不上报数据；
+
+/*
+ Set the California Consumer Privacy Act (CCPA) value;
+ Must be called before start TaurusX SDK;
+ 0 California users accept reported data;
+ 1 do not report data
+ */
 - (void)setCCPADoNotSell:(NSInteger)level;
-// 0 表明不是儿童；1 表明是儿童
+
+/*
+ Set the Children's Online Privacy Protection Act value;
+ Must be called before start TaurusX SDK;
+ 0 is not a child;
+ 1 is a child
+ */
 - (void)setCOPPAIsAgeRestrictedUser:(NSInteger)level;
-//0 设备数据允许上报 ；1 设备数据不允许上报；
+
+/*
+ Set the Lei Geral de Proteção de Dados (LGPD) value;
+ Must be called before start TaurusX SDK;
+ 0 The device data is allowed to be reported;
+ 1 The device data is not allowed to be reported
+ */
 - (void)setLGPDConsent:(NSInteger)level;
 
+/*
+ Set mediation identifier by mediation adapter
+ */
 - (void)setChannel:(NSString*)channel;
 
+/*
+ Set the host app icon,it will be displayed in Splash Ad.
+ */
 - (void)setAppIcon:(NSString*)iconName;
 
+/*
+ * A toggle for debug logging for the SDK.
+ * This is set to NO by default. Set it to NO if you want the SDK to be silent (this is recommended for App Store submissions).
+ * If set to YES TaurusX messages will appear in the standard application log which is accessible via the console. All TaurusX log messages are prefixed
+ * with the [TaurusX] tag.
+ */
 - (void)setDebugLogEnable:(BOOL)enable;
 
 @end
